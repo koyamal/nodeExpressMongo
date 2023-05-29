c34d742c9f39331fe0d5e8cba3a3ebc39fc64c55
koyamal <tanakamaruko1121n@gmail.com>
2023-05-29 20:56:16 +0900

    Use writeFile

--- lec17/write.js
+++ lec17/write.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 
-const data = "Hello, Node.js";
+const data = "Hello! Node.js!";
 fs.writeFile("./data/hello.txt", data, (error) => {
   if (error) {
     console.log(error);
     return;
   }
-  console.log("Done");
+  console.log("Done.");
 });
