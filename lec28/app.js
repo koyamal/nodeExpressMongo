21e4c0d1fce750d003e30d5871edc448b85d48d8
koyamal <tanakamaruko1121n@gmail.com>
2023-05-30 15:33:04 +0900

    Use params in url

--- lec28/app.js
+++ lec28/app.js
@@ -5,4 +5,9 @@ app.get("/home/index", (req, res) => {
   res.status(200).send("Hello, Express. /home/index");
 });
 
+app.get("/user/:id", (req, res) => {
+  console.log(req.params.id);
+  res.status(200).send(`Hello, Express. /user/${req.params.id}`);
+})
+
 app.listen(3000);
