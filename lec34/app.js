c84affadb5dc252c31f527512fdd40e1bd2cf986
koyamal <tanakamaruko1121n@gmail.com>
2023-05-31 18:20:45 +0900

    Check req

---
+++ lec34/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.all("/", (req, res) => {
+  let body = "";
+  console.log(req.method);
+  console.log(req.url);
+  console.log(JSON.stringify(req.headers));
+  req.on("data", (chunk) => {
+    body += chunk;
+  });
+  req.on("end", () => {
+    console.log(body);
+  });
+  res.send("OK");
+});
+
+app.listen(3000);
