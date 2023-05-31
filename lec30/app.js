5164100741b91731c6536d71932c29027d548739
koyamal <tanakamaruko1121n@gmail.com>
2023-05-31 15:01:40 +0900

    Install and Use hjs template

--- lec30/app.js
+++ lec30/app.js
@@ -3,13 +3,13 @@ const app = express();
 
 app.set('views', './views')
 // app.set("view engine", "pug");
-app.set("view engine", "ejs");
+// app.set("view engine", "ejs");
+app.set("view engine", "hjs");
 
 app.get("/", (req, res) => {
   res
     .status(200)
-    .render("index.ejs", {title: "Web App deploy", message: "Hello"})
-    .send(data);
+    .render("index.hjs", {title: "Web App deploy", message: "Hello"});
 });
 
 app.listen(3000);
