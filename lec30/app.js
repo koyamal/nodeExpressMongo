43b82b2a1026cccd67b4b9a8ab4f88ccdf9ad40d
koyamal <tanakamaruko1121n@gmail.com>
2023-05-31 15:03:54 +0900

    Install and Use hbs template

--- lec30/app.js
+++ lec30/app.js
@@ -4,12 +4,13 @@ const app = express();
 app.set('views', './views')
 // app.set("view engine", "pug");
 // app.set("view engine", "ejs");
-app.set("view engine", "hjs");
+// app.set("view engine", "hjs");
+app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
   res
     .status(200)
-    .render("index.hjs", {title: "Web App deploy", message: "Hello"});
+    .render("index.hbs", {title: "Web App deploy", message: "Hello"});
 });
 
 app.listen(3000);
