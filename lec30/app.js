12999e7bd783770470487c0a475de4835da3447a
koyamal <tanakamaruko1121n@gmail.com>
2023-05-31 14:53:24 +0900

    Install and Use pug template

--- lec30/app.js
+++ lec30/app.js
@@ -1,19 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.set('views', './views')
+app.set("view engine", "pug");
+
 app.get("/", (req, res) => {
-  let data = "<!DOCTYPE html>\r\n";
-  data += "<html>\r\n";
-  data += "  <head>\r\n";
-  data += "    <meta charset='UTF-8'>\r\n";
-  data += "    <title>startup</title>\r\n";
-  data += "  </head>\r\n";
-  data += "  <body>\r\n";
-  data += "    <h1>Hello World</h1>\r\n";
-  data += "    <p>Hello World</p>\r\n";
-  data += "  </body>\r\n";
-  data += "</html>";
-  res.status(200).send(data);
+  res
+    .status(200)
+    .render("index.pug", {title: "Web App deploy", message: "Hello"})
+    .send(data);
 });
 
 app.listen(3000);
