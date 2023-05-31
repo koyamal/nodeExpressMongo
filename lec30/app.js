56eddd7bbf4f5eef0f90d23866adbda981a62055
koyamal <tanakamaruko1121n@gmail.com>
2023-05-31 14:57:47 +0900

    Install and Use ejs template

--- lec30/app.js
+++ lec30/app.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 
 app.set('views', './views')
-app.set("view engine", "pug");
+// app.set("view engine", "pug");
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res
     .status(200)
-    .render("index.pug", {title: "Web App deploy", message: "Hello"})
+    .render("index.ejs", {title: "Web App deploy", message: "Hello"})
     .send(data);
 });
 
