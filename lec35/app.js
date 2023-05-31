1369499ffd467bcf0c05d3f06070d7c1ebb4fc31
koyamal <tanakamaruko1121n@gmail.com>
2023-05-31 18:45:44 +0900

    Check cwd in debug

--- lec35/app.js
+++ lec35/app.js
@@ -2,13 +2,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+app.set('views', './views');
+console.log(__dirname);
+console.log(process.cwd());
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.render(__dirname + "/views/index.ejs");
+  res.render("index.ejs");
 });
 
 app.post("/", (req, res) => {
