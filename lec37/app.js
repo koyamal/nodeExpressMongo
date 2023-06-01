84ed530d66ed9893e3494c700311f9e5b56a14f1
koyamal <tanakamaruko1121n@gmail.com>
2023-06-01 11:18:07 +0900

    Get and Set header

---
+++ lec37/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  console.log(req.get("user-agent"));
+  res.set("Cache-Control", "no-cache");
+  res.set("Pragma", "no-cache");
+  res.render("index.ejs");
+});
+
+app.listen(3000);
