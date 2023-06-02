8c93fdd0f10b1a0ebe052dbc585bfdc1b4d91b24
koyamal <tanakamaruko1121n@gmail.com>
2023-06-02 17:31:31 +0900

    Update gulpfile

--- weblog/gulpfile.js
+++ weblog/gulpfile.js
@@ -1,4 +1,11 @@
-const gulp = require("gulp");
+console.log("yello");
+// const gulp = require("gulp");
+import gulp from "gulp";
+import requireDir from "require-dir";
+// const load = require("require-dir");
+
+// console.log(requireDir("./gulp/task", { recurse: true}));
+console.log(requireDir("./gulp", { recurse: true}));
 
 gulp.task("default", () => {
   console.log("Hello, Gulp");
