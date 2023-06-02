4dae0e156fc86a0b97401f7b0949dd6618547acd
koyamal <tanakamaruko1121n@gmail.com>
2023-06-02 17:45:15 +0900

    Create check require/import effect

---
+++ weblog/index.js
@@ -0,0 +1,5 @@
+import requireDir from "require-dir";
+// const load = require("require-dir");
+
+// console.log(load("./gulp/task", { recurse: true}));
+console.log(requireDir("./gulp", { recurse: true}));
