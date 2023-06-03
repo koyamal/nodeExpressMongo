85a95d8d4283f18e41b683ac330e6b8ae761cf79
koyamal <tanakamaruko1121n@gmail.com>
2023-06-03 17:55:10 +0900

    Create midleware(myLogger)

--- express/myApp01/app.js
+++ express/myApp01/app.js
@@ -4,24 +4,24 @@ const birdsRouter = require("./routes/birds");
 
 const app = express();
 
-app.use('/static', express.static('public'));
-app.use(express.static('pictures'));
+// app.use('/static', express.static('public'));
+// app.use(express.static('pictures'));
 
-app.get("/", (req, res) => {
-  res.send("Hello, Express: GET");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello, Express: GET");
+// });
 
-app.post("/", (req, res) => {
-  res.send("Hello, Express: POST");
-});
+// app.post("/", (req, res) => {
+//   res.send("Hello, Express: POST");
+// });
 
-app.put("/", (req, res) => {
-  res.send("Hello, Express: PUT");
-});
+// app.put("/", (req, res) => {
+//   res.send("Hello, Express: PUT");
+// });
 
-app.delete("/", (req, res) => {
-  res.send("Hello, Express: GELETE");
-});
+// app.delete("/", (req, res) => {
+//   res.send("Hello, Express: GELETE");
+// });
 
 // app.get('/ab+cd', (req, res) => {
 //   res.send('/ab+cd');
@@ -81,6 +81,17 @@ app.delete("/", (req, res) => {
 //   .post((req, res) => { res.send("POST"); })
 //   .put((req, res) => { res.send("PUT"); });
 
-app.use("/birds", birdsRouter);
+// app.use("/birds", birdsRouter);
+
+const myLogger = function(req, res, next) {
+  console.log('LOGGED');
+  next();
+}
+
+app.use(myLogger);
+
+app.get("/", (req, res) => {
+  res.send("Hello, World");
+});
 
 app.listen(3000);
