7fa4a95572a7693a3cdcbcbfac2bf58a17ba408b
koyamal <tanakamaruko1121n@gmail.com>
2023-06-03 16:46:10 +0900

    Check Route parameters with req.params

--- express/myApp01/app.js
+++ express/myApp01/app.js
@@ -21,16 +21,22 @@ app.delete("/", (req, res) => {
   res.send("Hello, Express: GELETE");
 });
 
-app.get('/ab+cd', (req, res) => {
-  res.send('/ab+cd');
-});
+// app.get('/ab+cd', (req, res) => {
+//   res.send('/ab+cd');
+// });
 
-app.get('/ab*cd', (req, res) => {
-  res.send('/ab*cd');
-});
+// app.get('/ab*cd', (req, res) => {
+//   res.send('/ab*cd');
+// });
 
-app.get(/a/, (req, res) => {
-  res.send('/a/');
+// app.get(/a/, (req, res) => {
+//   res.send('/a/');
+// });
+
+app.get('/users/:userId/books/:bookId', (req, res) => {
+  res.send(req.params);
+  // res.send(req.params.userId);
+  // res.send(req.params.bookId);
 });
 
 app.listen(3000);
