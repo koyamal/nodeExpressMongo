b3d3fd7dd660b6584cfb76717240a989b212e7a8
koyamal <tanakamaruko1121n@gmail.com>
2023-06-03 10:37:46 +0900

    Check Order of static file displayed

--- express/myApp01/app.js
+++ express/myApp01/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.static('pictures'));
 
 app.get("/", (req, res) => {
   res.send("Hello, Express: GET");
