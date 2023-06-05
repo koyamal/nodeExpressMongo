9b32c184c994ee60b8b02da69b4dc875cd5cc20d
koyamal <tanakamaruko1121n@gmail.com>
2023-06-05 15:00:27 +0900

    Add next for callback of fs.writeFile

--- express/myApp01/app.js
+++ express/myApp01/app.js
@@ -7,14 +7,23 @@ const app = express();
 //   throw new Error("BROKEN");
 // });
 
-app.get("/", (req, rex, next) => {
-  fs.readFile("/file-does-not-exist", (err, data) => {
-    if(err){
-      next(err);
-    } else {
-      res.send(data);
-    }
-  })
-});
+// app.get("/", (req, rex, next) => {
+//   fs.readFile("/file-does-not-exist", (err, data) => {
+//     if(err){
+//       next(err);
+//     } else {
+//       res.send(data);
+//     }
+//   })
+// });
+
+app.get('/', [
+  function (req, res, next) {
+    fs.writeFile('/inaccessible-path', 'data', next)
+  },
+  function (req, res) {
+    res.send('OK')
+  }
+]);
 
 app.listen(3000);
