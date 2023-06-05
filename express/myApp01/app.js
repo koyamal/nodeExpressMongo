1b8c3a721b015eb7890bbc6111df44a7211e512a
koyamal <tanakamaruko1121n@gmail.com>
2023-06-05 13:15:41 +0900

    Create testRouter for test

--- express/myApp01/app.js
+++ express/myApp01/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 
+const router = require("./routes/testRouter");
 const sampleRouter = require("./routes/sample");
 const routerRouter = require("./routes/useRouter");
 
 const app = express();
+
+app.use('/', router)
 
 app.get("/", (req, res) => { res.send("Hello, World"); })
 
