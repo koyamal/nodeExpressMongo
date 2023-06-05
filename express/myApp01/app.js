9dcf0d1153bce2a5b83f9e7c7189968aeb527c18
koyamal <tanakamaruko1121n@gmail.com>
2023-06-05 11:18:19 +0900

    Use next("route")

--- express/myApp01/app.js
+++ express/myApp01/app.js
@@ -23,4 +23,14 @@ app.get("/user/:id", (req, res) => {
   res.send(`This is User's(ID: ${req.params.id}) page`);
 });
 
+app.get("/home/:id", (req, res, next) => {
+  req.params.id === '0'? next('route'): next();
+}, (req, res, next) => {
+  res.send('not 0');
+});
+
+app.get("/home/:id", (req, res) => {
+  res.send("0");
+});
+
 app.listen(3000);
