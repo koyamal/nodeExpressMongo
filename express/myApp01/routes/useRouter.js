4cc5fb390e23e3865dcad5d3f480a02aa3c50ca1
koyamal <tanakamaruko1121n@gmail.com>
2023-06-05 13:15:25 +0900

    Edit to toggle route/router

--- express/myApp01/routes/useRouter.js
+++ express/myApp01/routes/useRouter.js
@@ -3,7 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/user/:id", (req, res, next) => {
-  if (req.params.id === '0') next('route')
+  if (req.params.id === '0') {
+    console.log("next('route(r)'");
+    next("route");
+    // next("router");
+  }
   else next()
 }, (req, res, next) => {
   console.log("next()");
