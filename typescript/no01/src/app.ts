4f9ac3668ed0eaf7121a967f16376372a26c909d
koyamal <tanakamaruko1121n@gmail.com>
2023-06-06 18:37:58 +0900

    Check number/string/boolean

--- typescript/no01/src/app.ts
+++ typescript/no01/src/app.ts
@@ -1,6 +1,21 @@
-const num: number = 1234;
-function identity(num: number, num2: number): number {
-  return num + num2;
-}
+// const num: number = 1234;
+// function identity(num: number, num2: number): number {
+//   return num + num2;
+// }
 
-console.log(identity(num, 3));
+// console.log(identity(num, 3));
+
+let num: number;
+let str: string;
+let bool: boolean;
+
+num = 123;
+num = 123.456;
+num = '123';
+
+str = '123';
+str = 123;
+
+bool = true;
+bool = false;
+bool = "false";
