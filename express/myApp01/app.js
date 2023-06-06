716c06c1c44a1116aa55efdebe916727d51addec
koyamal <tanakamaruko1121n@gmail.com>
2023-06-06 10:54:28 +0900

    Use chain of handlers

--- express/myApp01/app.js
+++ express/myApp01/app.js
@@ -26,10 +26,23 @@ const app = express();
 //   }
 // ])
 
-app.get("/", (req, res, next) => {
-  Promise.resolve().then(() => {
-    throw new Error("BROKEN");
-  }).catch(next);
-});
+// app.get("/", (req, res, next) => {
+//   Promise.resolve().then(() => {
+//     throw new Error("BROKEN");
+//   }).catch(next);
+// });
+
+app.get("/", [
+  function(req, res, next) {
+    fs.readFile("/maybe-valid-file", "utf-8", (err, data) => {
+      res.locals.data = data;
+      next(err);
+    })
+  },
+  function(req, res) {
+    res.locals.data = res.locals.data.split(",")[1];
+    res.send(res.locals.data);
+  }
+])
 
 app.listen(3000);
