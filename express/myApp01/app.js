bb091e56d0bef69ab761f2147422646e9006da70
koyamal <tanakamaruko1121n@gmail.com>
2023-06-06 11:08:45 +0900

    Delete

--- express/myApp01/app.js
+++ express/myApp01/app.js
@@ -3,35 +3,6 @@ const fs = require("fs");
 
 const app = express();
 
-// app.get("/", (req, res) => {
-//   throw new Error("BROKEN");
-// });
-
-// app.get("/", (req, rex, next) => {
-//   fs.readFile("/file-does-not-exist", (err, data) => {
-//     if(err){
-//       next(err);
-//     } else {
-//       res.send(data);
-//     }
-//   })
-// });
-
-// app.get('/', [
-//   function (req, res, next) {
-//     fs.writeFile('/inaccessible-path', 'data', next)
-//   },
-//   function (req, res) {
-//     res.send('OK')
-//   }
-// ])
-
-// app.get("/", (req, res, next) => {
-//   Promise.resolve().then(() => {
-//     throw new Error("BROKEN");
-//   }).catch(next);
-// });
-
 app.get("/", [
   function(req, res, next) {
     fs.readFile("/maybe-valid-file", "utf-8", (err, data) => {
