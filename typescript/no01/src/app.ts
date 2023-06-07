35e8b4be3e1e6e4945fc15261cbceeafe6ff17d3
koyamal <tanakamaruko1121n@gmail.com>
2023-06-07 10:29:22 +0900

    Use Union Type

--- typescript/no01/src/app.ts
+++ typescript/no01/src/app.ts
@@ -1,25 +1,13 @@
-function reverse<T>(items: T[]): T[] {
-  const toreturn = [];
-  for (let i = items.length - 1; i >= 0; i--) {
-    toreturn.push(items[i]);
+function formatCommandLine(command: string[]|string): void{
+  let line = "";
+  if (typeof command === "string"){
+    line = command.trim();
+  }else {
+    line = command.join(" ").trim();
   }
-  return toreturn;
-}
 
-const sample = [1, 2, 3];
-const reversed = reverse(sample);
+  console.log(line);
+};
 
-console.log(reversed);
-
-reversed[0] = 10;
-// reversed[1] = "a";
-
-const sample2 = ["1", "2", "3"];
-const reversed2 = reverse(sample2);
-
-console.log(reversed2);
-
-reversed2[1] = "a";
-reversed2[2] = 2;
-
-
+formatCommandLine("pwd");
+formatCommandLine(["cd", "~"]);
