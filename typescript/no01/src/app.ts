5cd322af91f78bf8e44f1daea880a39b236503f7
koyamal <tanakamaruko1121n@gmail.com>
2023-06-07 10:10:54 +0900

    Use T

--- typescript/no01/src/app.ts
+++ typescript/no01/src/app.ts
@@ -1,5 +1,14 @@
-function log(message: string): void {
-  console.log(message);
+function reverse<T>(items: T[]): T[] {
+  const toreturn = [];
+  for (let i = items.length - 1; i >= 0; i--) {
+    toreturn.push(items[i]);
+  }
+  return toreturn;
 }
 
-log("Hello");
+const sample = [1, 2, 3];
+const reversed = reverse(sample);
+
+console.log(reversed);
+
+reversed[1] = "a";
