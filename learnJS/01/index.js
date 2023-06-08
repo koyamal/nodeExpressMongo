d9ca9c58e04b3f3a03d409da1e0af302354b6e17
koyamal <tanakamaruko1121n@gmail.com>
2023-06-08 14:09:58 +0900

    Use this in callback function

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,19 +1,17 @@
-window.name = "Jhon"
+window.name = "Jhon";
 
 const person = {
   name: "Tom",
   hello: function() {
-    console.log("I'm " + this.name);
-    a();
+    console.log("Hello, " , this.name);
   }
 }
 
 person.hello();
 
-const ref = person.hello;
 
-ref();
-
-function a() {
-  console.log("Hello " + this.name);
+function fn(ref) {
+  ref();
 }
+
+fn(person.hello);
