d6faffc5d022b47f0179192525089152dda3e59c
koyamal <tanakamaruko1121n@gmail.com>
2023-06-08 15:56:51 +0900

    Use prototype

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,8 +1,20 @@
 function Person(name, age) {
   this.name = name;
   this.age = age;
+  this.hello = function() {
+    console.log("Hello " + this.name);
+  }
+}
+
+Person.prototype.hello = function() {
+  console.log("Hello " + this.name);
 }
 
 const bob = new Person("Bob", 18);
 const tom = new Person("Tom", 23);
 const sun = new Person("Sun", 16);
+
+bob.hello();
+tom.hello();
+
+console.log(Person.prototype === bob.__proto__);
