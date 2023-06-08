1be4d87d9bc4bd81c99f70351cf4bab9c5efa0a7
koyamal <tanakamaruko1121n@gmail.com>
2023-06-08 11:13:45 +0900

    Use args

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -6,11 +6,12 @@
 
 // fn(1);
 
-const fn2 = function(a, b = 1) {
+const fn2 = function(...args) {
+  console.log(args);
   console.log(arguments);
   console.log(arguments[0]);
   console.log(arguments[1]);
-  console.log(a, b);
+  // console.log(a, b);
 }
 
 fn2(1, null);
