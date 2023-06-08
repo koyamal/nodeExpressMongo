0d753eec52f2f14aaa149e75bfaaa295b3b78b4a
koyamal <tanakamaruko1121n@gmail.com>
2023-06-08 14:06:47 +0900

    Use this method of object/function

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,12 +1,19 @@
+window.name = "Jhon"
+
 const person = {
   name: "Tom",
   hello: function() {
     console.log("I'm " + this.name);
+    a();
   }
 }
 
 person.hello();
 
-const hello = person.hello;
+const ref = person.hello;
 
-hello();
+ref();
+
+function a() {
+  console.log("Hello " + this.name);
+}
