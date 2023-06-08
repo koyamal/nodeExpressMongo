a16df2566006208d5031d88bbfa34c4f4682ff0c
koyamal <tanakamaruko1121n@gmail.com>
2023-06-08 11:10:42 +0900

    Use default value of arg

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,1 +1,15 @@
-console.log("Hello.");
+// console.log("Hello.");
+
+// function fn(a, b) {
+//   console.log(a, b);
+// }
+
+// fn(1);
+
+const fn2 = function(a, b = 1) {
+  console.log(a, b);
+}
+
+fn2(1, null);
+fn2(1);
+fn2(1, undefined);
