b7cbd5db4b03e83024f893406a51aea4d3043e88
koyamal <tanakamaruko1121n@gmail.com>
2023-06-09 12:43:54 +0900

    Use class

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,34 +1,23 @@
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  hello() {
+    console.log("Person: Hello " + this.name);
+  }
 }
 
-Person.prototype.hello = function() {
-  console.log("Person: Hello " + this.name);
-}
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
 
-Person.prototype.bye = function() {
-  console.log("Person: Bye " + this.name);
-}
+// Person.prototype.hello = function() {
+//   console.log("Person: Hello " + this.name);
+// }
 
-function Japanese(name, age) {
-  Person.call(this, name, age);
-}
-
-Japanese.prototype = Object.create(Person.prototype);
-
-Japanese.prototype.bye = function() {
-  console.log("Japanese: Sauyonara " + this.name);
-}
-
-Japanese.prototype.seeyou = function() {
-  console.log("Japanese: SeeYou " + this.name);
-}
-
-const taro = new Japanese("taro", 23);
-
-console.log(taro);
-
-taro.hello();
-taro.bye();
-taro.seeyou();
+const bob = new Person("Bob", 32);
+console.log(bob);
+bob.hello();
