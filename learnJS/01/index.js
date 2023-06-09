0c6845fde77f3b2b22bc560ce10a90c9fde75a18
koyamal <tanakamaruko1121n@gmail.com>
2023-06-09 10:37:48 +0900

    Check instanceof

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,23 +1,17 @@
 function F(a, b) {
   this.a = a;
   this.b = b;
-  return {};
+  // return {};
   // return 1
 }
 
 F.prototype.c = function() {}
 
-function newOpe(C, ...args) {
-  const _this = Object.create(C.prototype);
-  const result = C.apply(_this, args);
-  // console.log(result, _this)
-  if (typeof result === "object" && result !== null) {
-    return result;
-  }
+const instance = new F(1, 2);
 
-  return _this;
-}
-
-// const instance = new F(1, 2);
-const instance = newOpe(F, 1, 2);
 console.log(instance);
+
+console.log(instance instanceof F);
+console.log(instance instanceof Object);
+console.log(instance.__proto__ === F.prototype);
+console.log(instance.__proto__ === Object.prototype);
