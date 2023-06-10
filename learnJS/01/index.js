9f27a2bf5e5a6a9eb1670c5c5ce7ae948d1c968a
koyamal <tanakamaruko1121n@gmail.com>
2023-06-10 10:48:09 +0900

    Create genIterator

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,34 +1,20 @@
-const map = new Map();
+function genIterator(macx) {
+  let i = 0;
 
-const key1 = {};
-
-map.set(key1, "value1");
-
-console.log(map.get(key1));
-
-const key2 = function() {};
-map.set(key2, "value2");
-
-console.log(map.get(key2));
-
-let key3;
-map.set(key3 = 0, 'value3');
-console.log(map.get(0));
-
-console.log(map);
-
-for(const m of map) {
-  console.log(m);
+  return {
+    next: function() {
+      return {
+        done: false,
+        value: i++,
+      }
+    }
+  }
 }
 
-for(const [k, v] of map) {
-  console.log(k, v);
-}
+const it = genIterator();
 
-const s = new Set();
-s.add(key1);
-s.add(key1);
-
-for(let k of s) {
-  console.log(k);
-}
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
