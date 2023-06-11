6e0b50ca4861e8ac7da804c4625f36b66775f3fd
koyamal <tanakamaruko1121n@gmail.com>
2023-06-11 10:30:06 +0900

    Add button

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,34 +1,10 @@
-function genIterator(max = 10) {
-  let i = 0;
-
-  return {
-    next: function() {
-      if(i > max) {
-        return {
-          done: true,
-        }
-      } else {
-        return {
-          done: false,
-          value: i++,
-        }
-      }
-    }
-  }
+function sleep(ms) {
+  const startTime = new Date();
+  while(new Date() - startTime < ms);
+  console.log('sleep done');
 }
 
-const it = genIterator(10);
-
-// let a = it.next();
-// while(!a.done) {
-//   console.log(a.value);
-//   a = it.next();
-// }
-
-const obj = {
-  [Symbol.iterator]: genIterator.bind(null, 10)
-}
-
-for(const i of obj) {
-  console.log(i);
-}
+const btn = document.querySelector('button');
+btn.addEventListener('click', function(){
+  console.log('button clicked');
+})
