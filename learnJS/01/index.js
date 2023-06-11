b8e1ad2c7d72d521b0288547c1d82ea61d18722c
koyamal <tanakamaruko1121n@gmail.com>
2023-06-11 16:49:40 +0900

    Use reject/catch

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,8 +1,14 @@
 new Promise(function(resolve, reject) {
   console.log("Promise");
   resolve();
+  // reject();
 }).then(function(){
-  console.log("then");
+  console.log("then 1");
+  throw new Error();
+}).then(function(){
+  console.log("then 2");
+}).catch(function(){
+  console.log("catch");
 });
 
 console.log("Global end");
