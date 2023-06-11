68675245cb33d53016a308e36367c25429854ef4
koyamal <tanakamaruko1121n@gmail.com>
2023-06-11 16:20:49 +0900

    Check inside fn2 timing

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,12 +1,15 @@
 const btn = document.querySelector("button");
 btn.addEventListener("click", function task2() {
   console.log("task2 done");
+  setTimeout(() => {
+    console.log("Inside task2 done");
+  }, 1000);
 });
 
 function a() {
   setTimeout(function task1(){
     console.log("task1 done");
-  }, 4000);
+  }, 6000);
 
   const startTime = new Date();
   while(new Date() - startTime < 2000);
