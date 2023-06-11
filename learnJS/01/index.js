e3c78e0827a4831d383f6db62e7f074bfe9a3075
koyamal <tanakamaruko1121n@gmail.com>
2023-06-11 17:34:53 +0900

    Delete

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -12,18 +12,4 @@ Promise.allSettled([sleep(2), sleep(3), sleep(4)]).then(function(data) {
   console.log(data);
 }).catch(function(e) {
   console.log("Hello");
-  // console.log(e);
 });
-
-// sleep(0).then(function(val) {
-//   return sleep(val);
-// }).then(function(val) {
-//   return Promise.all([sleep(2), sleep(3), sleep(4)])
-// }).then(function(val) {
-//   console.log(val);
-//   return sleep(val[0]);
-// }).then(function(val) {
-//   return sleep(val);
-// }).then(function(val) {
-//   return sleep(val);
-// });
