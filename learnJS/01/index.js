5fe1902cc2612476662819602c156b01868dc2b5
koyamal <tanakamaruko1121n@gmail.com>
2023-06-11 15:30:12 +0900

    Add button

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,24 +1,5 @@
-function sleep(ms) {
-  const startTime = new Date();
-  while(new Date() - startTime < ms);
-  console.log('sleep done');
-}
+const btn = document.querySelector("button");
+btn.addEventListener("click", function task2() {
+  console.log("task2 done");
+});
 
-const btn = document.querySelector('button');
-btn.addEventListener('click', function(){
-  console.log('button clicked');
-})
-
-
-// sleep(3000);
-
-setTimeout(function() {
-  console.log("no1");
-  setTimeout(function() {
-    console.log("no3");
-    sleep(4000);
-    console.log("no4");
-  }, 4000);
-  sleep(3000);
-  console.log("no2");
-}, 2000);
