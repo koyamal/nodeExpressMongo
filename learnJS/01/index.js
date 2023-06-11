b21581cb9b2ab38145cb09fdd37a5fb71b98c32c
koyamal <tanakamaruko1121n@gmail.com>
2023-06-11 16:16:28 +0900

    Use setTimeout and while

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -3,3 +3,15 @@ btn.addEventListener("click", function task2() {
   console.log("task2 done");
 });
 
+function a() {
+  setTimeout(function task1(){
+    console.log("task1 done");
+  }, 4000);
+
+  const startTime = new Date();
+  while(new Date() - startTime < 2000);
+
+  console.log("fn a done");
+}
+
+a();
