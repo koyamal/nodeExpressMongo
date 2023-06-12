7fe7470214ca852a593cb6401dce55678267473f
koyamal <tanakamaruko1121n@gmail.com>
2023-06-12 11:57:26 +0900

    Add useFetch

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,3 +1,19 @@
+async function useFetch() {
+  const response = await fetch("./users.json");
+  const users = await response.json();
+
+  return users;
+}
+
+async function init() {
+  const users = await useFetch();
+  users.forEach((user) => {
+    console.log(`I'm ${user.name}, ${user.age} year's old.`);
+  });
+}
+
+init();
+
 try {
   console.log("hello");
   throw new Error();
