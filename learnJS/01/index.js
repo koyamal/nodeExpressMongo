9e5fc7f4d9663d4a6b085ad7fc5ecf5875156ec6
koyamal <tanakamaruko1121n@gmail.com>
2023-06-12 10:23:18 +0900

    Use async/await

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,40 +1,19 @@
-console.log("global start");
-
-new Promise(function promise(resolve) {
-  console.log("promise");
-
-  setTimeout(function task1() {
-    console.log("task1");
-    resolve();
+function sleep(val) {
+  return new Promise(function(resolve) {
+    setTimeout(function() {
+      console.log(val++);
+      resolve(val);
+    }, 1000);
   });
+}
 
-  setTimeout(function task3(){
-    console.log("task3");
-    queueMicrotask(function job6() {
-      console.log("job6");
-    });
-  });
-}).then(function job1() {
-  console.log("job1");
-  setTimeout(function task2(){
-    console.log("task2");
+async function init() {
+  let val = await sleep(0);
+  val = await sleep(val);
+  val = await sleep(val);
+  val = await sleep(val);
+  val = await sleep(val);
+  val = await sleep(val);
+}
 
-    const p = Promise.resolve();
-    p.then(function job7() {
-      console.log("job7");
-    });
-    queueMicrotask(function job5() {
-      console.log("job5");
-    });
-  });
-
-  queueMicrotask(function job4() {
-    console.log("job4");
-  });
-}).then(function job2() {
-  console.log("job2");
-}).then(function job3() {
-  console.log("job3");
-});
-
-console.log("global end");
+init();
