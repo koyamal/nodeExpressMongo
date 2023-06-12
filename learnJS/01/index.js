d68506e7f1cebf6d36ef2c764dbdc2c969cb63bb
koyamal <tanakamaruko1121n@gmail.com>
2023-06-12 11:51:44 +0900

    Use async/await instead of then

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,11 +1,22 @@
 // console.log(window.fetch("./users.json"));
 
-fetch("./users.json").then(function(response) {
-  console.log(response);
-  return response.json();
-}).then(function(json){
-  console.log(json);
-  json.forEach(data => {
-    console.log(`I'm ${data.name}, ${data.age} year's old.`);
+// fetch("./users.json").then(function(response) {
+//   console.log(response);
+//   return response.json();
+// }).then(function(json){
+//   console.log(json);
+//   json.forEach(data => {
+//     console.log(`I'm ${data.name}, ${data.age} year's old.`);
+//   });
+// });
+
+
+async function useFetch() {
+  const response = await fetch("./users.json");
+  const users = await response.json();
+  users.forEach((user) =>{
+    console.log(`I'm ${user.name}, ${user.age} year's old.`);
   });
-});
+}
+
+useFetch();
