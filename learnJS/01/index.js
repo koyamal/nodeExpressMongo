fa0f47b371318b8f228afd37ba3bccd7c9308161
koyamal <tanakamaruko1121n@gmail.com>
2023-06-12 09:56:57 +0900

    Call resolve in setTimeout callback

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,11 +1,10 @@
-console.log("global start");
-setTimeout(function task1() {
-  console.log("task1");
-});
-
 new Promise(function promise(resolve) {
   console.log("promise");
-  resolve();
+
+  setTimeout(function task1() {
+    console.log("task1");
+    resolve();
+  });
 }).then(function job1() {
   console.log("job1");
 });
