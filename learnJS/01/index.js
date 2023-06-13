9b94dcc0b6c344a13061799bdd968d0400ca848c
koyamal <tanakamaruko1121n@gmail.com>
2023-06-13 14:56:30 +0900

    Use closure

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,50 +1,13 @@
-const person = {
-  hello: function() {
-    return "hello tom";
+function genIncrement(val) {
+  return function() {
+    console.log(val++);
   }
 }
 
-setTimeout(() => {
-  console.log(person.hello());
-}, 1000);
-
-// setTimeout(() => {
-//   alert(person.hello());
-// }, 1000);
-
-const obj = {
-
-}
-obj.greeting = function() {
-  console.log("hello");
-}
-
-function after1s(callback) {
-  setTimeout(callback, 1000);
-}
-
-after1s(obj.greeting);
-
-obj.greeting = function() {
-  console.log("hey");
-}
-
-function calcFactory(val, cb) {
-  return {
-    plus: function(target) {
-      const newVal = val + target;
-      cb(`${val} + ${target} = ${newVal}`);
-      val = newVal;
-    },
-    minus: function(target) {
-      const newVal = val - target;
-      cb(`${val} - ${target} = ${newVal}`);
-      val = newVal;
-    }
-  }
-}
-
-const calc = calcFactory(10, console.log);
-calc.plus(5);
-calc.minus(2);
-console.log(calc);
+const increment = genIncrement(10);
+increment();
+increment();
+increment();
+increment();
+increment();
+increment();
