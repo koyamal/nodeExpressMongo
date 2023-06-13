40c27620a487c5d2d003006d39f78d8e7eedb1c3
koyamal <tanakamaruko1121n@gmail.com>
2023-06-13 16:49:32 +0900

    Use closure

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,54 +1,7 @@
-const person = {
-  name: "Tom",
-  bye: function () {
-    console.log("Bye " + this.name);
-  },
-  hello: function(greeting) {
-    console.log(greeting + " " + this.name);
-    return greeting + " " + this.name;
-  },
-  hello1sa: function() {
-    setTimeout(() => {
-      console.log("Hello " + this.name);
-    },1000);
-  },
-  hello1sb: function() {
-    function a() {
-      console.log("Hello " + this.name);
-    }
-    setTimeout(a.bind(this), 1000);
-  },
-  hello1sc: function() {
-    const this_ = this;
-    setTimeout(() => {
-      console.log("Hello " + this_.name);
-    }, 1000);
-  },
-  hello1sd: function() {
-    setTimeout(this.hello.bind(this, "hello"), 1000);
-  },
-  hello1se: function() {
-    setTimeout(() => { this.hello("hello")}, 1000);
-  },
-  hello1sf: function() {
-    const _this = this;
-    setTimeout(function() {
-      _this.hello("hello");
-    }, 1000);
+function hello(name) {
+  return function() {
+    console.log("Hello " + name);
   }
 }
 
-// setTimeout(person.hello.bind(person, "hello"), 1000);
-// setTimeout(() => {
-//   person.hello("hello");
-// }, 1000);
-
-// setTimeout(person.bye.bind(person), 1000);
-
-
-person.hello1sa();
-person.hello1sb();
-person.hello1sc();
-person.hello1sd();
-person.hello1se();
-person.hello1sf();
+setTimeout(hello("Tom"), 1000);
