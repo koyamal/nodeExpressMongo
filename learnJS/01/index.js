1c9b520aa87885c072f9e346fcc227b30a18805e
koyamal <tanakamaruko1121n@gmail.com>
2023-06-13 11:26:24 +0900

    Check primitive value

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,28 +1,9 @@
-async function useFetch() {
-  const response = await fetch("./users.json");
-  if(response.ok) {
-    const users = await response.json();
-    if(!users.length) {
-      throw new Error("no data found");
-    }
-    return users;
-  } else {
-    throw new Error("Load data failed");
-  }
-}
+let a = "hello";
 
-async function init() {
-  try {
-    const users = await useFetch();
-    console.log(users);
-    users.forEach((user) => {
-      console.log(`I'm ${user.name}, ${user.age} year's old.`);
-    });
-  } catch(e) {
-    console.error(e.message);
-  } finally {
-    console.log("bye");
-  }
-}
+let b = a;
 
-init();
+console.log(a,b);
+
+b = "bye";
+
+console.log(a,b);
