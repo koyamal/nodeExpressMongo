2099a343c671ffab5ccd6b31062b54ff624c48ce
koyamal <tanakamaruko1121n@gmail.com>
2023-06-13 11:43:12 +0900

    Check equality of object

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -2,31 +2,8 @@ const a = {
   prop: 0
 }
 
-let { prop } = a;
-
-console.log(a.prop, prop);
-
-prop = 1;
-
-console.log(a.prop, prop);
-
-function fn(obj) {
-  let { prop } = obj;
-  prop = 1;
-  console.log(obj, prop);
+const b = {
+  prop: 0
 }
 
-fn(a);
-
-const c = {
-  prop1: {
-    prop2: 0
-  }
-}
-
-let { prop1 } = c;
-console.log(prop1);
-
-prop1.prop2 = 1;
-
-console.log(c, prop1);
+console.log(a === b);
