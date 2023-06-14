71720738b827307f7e2a8f508108ead6f495f015
koyamal <tanakamaruko1121n@gmail.com>
2023-06-14 09:17:59 +0900

    Refactoring code

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -2,6 +2,7 @@ class Calculator{
   constructor() {
     this.ans = 0;
     this.flag = null;
+    this.calcformula = () => {}
   }
   set(val) {
     if(!this.flag)
@@ -10,35 +11,54 @@ class Calculator{
       return this;
     }
     if(this.flag === "plus") {
-      this.ans += val;
-      console.log(this.ans);
+      this.calcformula(val);
+      // this.ans += val;
+      // console.log(this.ans);
     } else if (this.flag === "minus") {
-      this.ans -= val;
-      console.log(this.ans);
+      this.calcformula(val);
+      // this.ans -= val;
+      // console.log(this.ans);
     } else if (this.flag === "mutiply") {
-      this.ans *= val;
-      console.log(this.ans);
+      this.calcformula(val);
+      // this.ans *= val;
+      // console.log(this.ans);
     } else if (this.flag === "divide") {
-      this.ans /= val;
-      console.log(this.ans);
+      this.calcformula(val);
+      // this.ans /= val;
+      // console.log(this.ans);
     }
     return this;
   }
   plus() {
     this.flag = "plus";
-
+    this.calcformula = (val) => {
+      this.ans += val;
+      console.log(this.ans);
+    }
     return this;
   }
   minus() {
     this.flag = "minus";
+    this.calcformula = (val) => {
+      this.ans -= val;
+      console.log(this.ans);
+    }
     return this;
   }
   mutiply() {
     this.flag = "mutiply";
+    this.calcformula = (val) => {
+      this.ans *= val;
+      console.log(this.ans);
+    }
     return this;
   }
   divide() {
     this.flag = "divide";
+    this.calcformula = (val) => {
+      this.ans /= val;
+      console.log(this.ans);
+    }
     return this;
   }
 }
