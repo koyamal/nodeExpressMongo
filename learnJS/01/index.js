6530d8450e10d647dfaf6aaab40d56d4cc288d99
koyamal <tanakamaruko1121n@gmail.com>
2023-06-14 17:52:04 +0900

    Use catch

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,8 +1,13 @@
 new Promise(function(resolve, reject) {
   console.log("Promise");
-  resolve();
+  // resolve();
+  reject();
 }).then(function() {
   console.log("then");
+}).then(function() {
+  console.log("then");
+}).catch(function() {
+  console.log("catch");
 });
 
 console.log("global end");
