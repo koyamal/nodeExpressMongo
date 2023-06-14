63014435feaf97a833cee67327a7798c0f705077
koyamal <tanakamaruko1121n@gmail.com>
2023-06-14 17:07:10 +0900

    Use promise

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,31 +1,7 @@
-async function fetchFile(fileName) {
-  const response = await fetch(fileName);
-	const json = await response.json();
+console.log(fetch("users.json"));
 
-  return json;
-}
+const a = fetch("users.json").then((data) => {
+  console.log(data);
+});
 
-async function getUserInfo(id) {
-  return await fetchFile(`./json/user${id}.json`);
-}
-
-async function getFriendsList(id) {
-  return await fetchFile(`./json/friendsOf${id}.json`);
-}
-
-async function getMessage(id) {
-  return await fetchFile(`./json/message${id}.json`);
-}
-
-async function myFetch(id) {
-  const userInfo = await getUserInfo(id);
-  console.log(`--${userInfo.name}'s timeline--`);
-  const friendsList = await getFriendsList(userInfo.id);
-  friendsList.friendIds.forEach(async function(friendId) {
-    const friendInfo = await getUserInfo(friendId);
-    const latestMsg = await getMessage(friendInfo.latestMsgId);
-    console.log(`${friendInfo.name} says: ${latestMsg.message}`);
-  });
-}
-
-myFetch(1);
+console.log(a);
