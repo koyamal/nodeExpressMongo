eab0afca8b374266b8ebced7eac3b8b0c110fe2c
koyamal <tanakamaruko1121n@gmail.com>
2023-06-14 11:42:36 +0900

    Create TimeLine

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,8 +1,33 @@
-async function myFetch(fileName) {
-	const response = await fetch(`./json/${fileName}`);
+async function getUserInfo(id) {
+  const response = await fetch(`./json/user${id}.json`);
 	const json = await response.json();
-  console.log(json);
-	return json;
+
+  return json;
 }
 
-myFetch("user1.json");
+async function getFriendsList(id) {
+  const response = await fetch(`./json/friendsOf${id}.json`);
+  const json = await response.json();
+
+  return json;
+}
+
+async function getMessage(id) {
+  const response = await fetch(`./json/message${id}.json`);
+  const json = await response.json();
+
+  return json;
+}
+
+async function myFetch(id) {
+  const userInfo = await getUserInfo(id);
+  console.log(`--${userInfo.name}'s timeline--`);
+  const friendsList = await getFriendsList(userInfo.id);
+  friendsList.friendIds.forEach(async function(friendId) {
+    const friendInfo = await getUserInfo(friendId);
+    const latestMsg = await getMessage(friendInfo.latestMsgId);
+    console.log(`${friendInfo.name} says: ${latestMsg.message}`);
+  });
+}
+
+myFetch(1);
