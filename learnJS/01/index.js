d8ac758f19259501973c4c6805963233297b99e0
koyamal <tanakamaruko1121n@gmail.com>
2023-06-14 09:15:22 +0900

    Use chainemethod

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,15 +1,47 @@
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
+class Calculator{
+  constructor() {
+    this.ans = 0;
+    this.flag = null;
   }
+  set(val) {
+    if(!this.flag)
+    {
+      this.ans = val;
+      return this;
+    }
+    if(this.flag === "plus") {
+      this.ans += val;
+      console.log(this.ans);
+    } else if (this.flag === "minus") {
+      this.ans -= val;
+      console.log(this.ans);
+    } else if (this.flag === "mutiply") {
+      this.ans *= val;
+      console.log(this.ans);
+    } else if (this.flag === "divide") {
+      this.ans /= val;
+      console.log(this.ans);
+    }
+    return this;
+  }
+  plus() {
+    this.flag = "plus";
 
-  hello() {
-    console.log("Hello " + this.name);
+    return this;
+  }
+  minus() {
+    this.flag = "minus";
+    return this;
+  }
+  mutiply() {
+    this.flag = "mutiply";
+    return this;
+  }
+  divide() {
+    this.flag = "divide";
+    return this;
   }
 }
 
-const bob = new Person("Bob", 23);
-
-setTimeout(bob.hello.bind(bob), 1000);
-setTimeout(() => {bob.hello()}, 1000);
+const calc = new Calculator();
+calc.set(10).plus().set(3).minus().set(14).mutiply().set(15).divide().set(5);
