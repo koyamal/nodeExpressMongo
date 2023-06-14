f605f71a1d510016ab2d091b68d1a38e0c2509db
koyamal <tanakamaruko1121n@gmail.com>
2023-06-14 17:53:15 +0900

    Use finally

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,13 +1,16 @@
 new Promise(function(resolve, reject) {
   console.log("Promise");
-  // resolve();
-  reject();
+  resolve();
+  // reject();
 }).then(function() {
   console.log("then");
+  throw new Error();
 }).then(function() {
   console.log("then");
 }).catch(function() {
   console.log("catch");
+}).finally(function() {
+  console.log("finally");
 });
 
 console.log("global end");
