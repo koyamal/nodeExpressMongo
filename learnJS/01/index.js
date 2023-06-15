c411b57d5abc9588757d37c9943f22d6c524fbcb
koyamal <tanakamaruko1121n@gmail.com>
2023-06-15 18:40:14 +0900

    Check output of async function

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,23 +1,9 @@
-fetch('users.json').then(function(response) {
-  console.log(response);
-  return response.json();
-}).then(function(json) {
-  console.log(json);
-  json.forEach(data => {
-    console.log(`${data.name}, ${data.age}`);
-  });
-  console.log();
-  return json[0];
-}).then(function(json) {
-  console.log(json);
-});
+async function fetchFile(filePath) {
+  const res = await fetch(filePath);
+  const data = await res.json();
 
-async function fetchUsers() {
-  const res = await fetch("users.json");
-  const users = await res.json();
+  return data;
+};
 
-  console.log(users);
-  users.forEach(user => {
-    console.log(`${data.name}, ${data.age}`);
-  });
-}
+const data = fetchFile("users.json");
+console.log(data);
