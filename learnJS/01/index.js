bc1cbb5422270fed0ebc227f68d0bb7071c496f5
koyamal <tanakamaruko1121n@gmail.com>
2023-06-15 16:51:48 +0900

    Use fetch

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,47 +1,11 @@
-function sleep(val) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      console.log(val++);
-      resolve(val);
-    }, 1000);
-  });
+const a = fetch("./users.json");
+
+console.log(a);
+
+
+async function fn() {
+  const a = await fetch("./users.json");
+  console.log(a.json());
 }
 
-async function init() {
-  let val = await sleep(0);
-  val = await sleep(val);
-  val = await sleep(val);
-  val = await sleep(val);
-  val = await sleep(val);
-  // console.log(val);
-  // throw new Error("New Error");
-  console.log(sleep(10));
-  return val;
-}
-
-// init().then(function(val) {
-//   console.log("hello", val);
-// }).catch(function(e) {
-//   console.log(e);
-// });
-
-async function init2() {
-  const val =  init();
-  console.log("init2: ", val);
-}
-
-init2();
-
-// sleep(0).then(function(val) {
-//   return sleep(val);
-// }).then(function(val) {
-//   return sleep(val);
-// }).then(function(val) {
-//   sleep(val);
-//   return val;
-// }).then(function(val) {
-//   sleep(val);
-//   return sleep(val);
-// }).then(function(val) {
-//   return sleep(val);
-// });
+fn();
