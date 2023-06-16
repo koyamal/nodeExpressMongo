0a4e7840d173257137214eb77e0ed4c76651d1d3
koyamal <tanakamaruko1121n@gmail.com>
2023-06-16 15:17:37 +0900

    Create Japanese class extends Person class

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -8,7 +8,24 @@ class Person {
   sayHello() {
     console.log(`I'm ${this.name}, from ${this.countory}. Hello`);
   }
+
+  sayBye() {
+    console.log("Bye");
+  }
+}
+
+class Japanese extends Person {
+  constructor(name, age, countory) {
+    super(name, age, countory);
+  }
+  sayBye() {
+    console.log("さようなら");
+  }
 }
 
 const bob = new Person("bob", 43, "USA");
 bob.sayHello();
+bob.sayBye();
+
+const taro = new Japanese("taro", 12, "Japanese");
+taro.sayBye();
