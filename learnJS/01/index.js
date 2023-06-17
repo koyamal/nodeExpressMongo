51e02e877229ed4b872b56e41d47b61e48bab00d
koyamal <tanakamaruko1121n@gmail.com>
2023-06-17 16:45:06 +0900

    Use protopype

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -3,8 +3,13 @@ function Person(name, age) {
   this.age = age;
 }
 
+Person.prototype.hello = function() {
+  console.log("Hello " + this.name);
+}
+
 const bob = new Person("bob", 23);
 const tom = new Person("tom", 24);
 const sun = new Person("sun", 25);
 
 console.log(bob);
+bob.hello();
