dc17132216c3d6687fb3972b6920a3604bd8f335
koyamal <tanakamaruko1121n@gmail.com>
2023-06-17 16:41:28 +0900

    Use constructor function

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,38 +1,8 @@
-class Person {
-  constructor(name, age, countory) {
-    this.name = name;
-    this.age = age;
-    this.countory = countory;
-  }
-
-  hello = function() {
-    console.log("hello, ", this.name);
-  }
-  sayHello() {
-    console.log(`I'm ${this.name}, from ${this.countory}. Hello`);
-  }
-
-  sayBye() {
-    console.log("Bye");
-  }
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
 }
 
-class Japanese extends Person {
-  constructor(name, age, countory) {
-    super(name, age, countory);
-  }
-  sayBye() {
-    console.log("さようなら");
-  }
-}
-
-const bob = new Person("bob", 43, "USA");
-bob.sayHello();
-bob.sayBye();
+const bob = new Person("bob", 23);
 
 console.log(bob);
-
-const taro = new Japanese("taro", 12, "Japanese");
-taro.sayBye();
-
-console.log(taro);
