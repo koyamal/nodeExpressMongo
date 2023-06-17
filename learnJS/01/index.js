fff44b6c184b52a118ca09c7e34a907742d6c5f7
koyamal <tanakamaruko1121n@gmail.com>
2023-06-18 08:41:38 +0900

    Create Japanese class

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -18,10 +18,24 @@ class Person {
   }
 }
 
+class Japanese extends Person {
+  constructor(name, age, id) {
+    super(name, age, id);
+    this.country = "Japanese";
+  }
+
+  hello() {
+    console.log(`こんにちは、私は${this.name}です。`);
+  }
+}
+
 const addNum = numGenerator();
 
 const bob = new Person("bob", 23, addNum());
 const tom = new Person("tom", 32, addNum());
+const taro = new Japanese("太郎", 43, addNum());
 console.log(bob);
 console.log(tom);
+console.log(taro);
 bob.hello();
+taro.hello();
