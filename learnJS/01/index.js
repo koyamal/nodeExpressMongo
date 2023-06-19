b04ecd6761f75f148f3d73a34dae8280a7c7feb4
koyamal <tanakamaruko1121n@gmail.com>
2023-06-20 07:59:22 +0900

    Use Promise

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,58 +1,9 @@
-function Person (name, age) {
-  this.name = name;
-  this.age = age;
+new Promise((resolve, reject) => {
+  console.log("Hello ");
+  resolve();
+}).then(() => {
+  console.log("World");
 }
-
-Person.prototype.hello = function() {
-  console.log("Hello, I'm", this.name);
-}
-
-Person.prototype.bye = function() {
-  console.log("bye");
-}
-
-function Japanese(name, age) {
-  Person.call(this, name, age);
-  this.lang = "Japanese";
-}
-
-Japanese.prototype = Object.create(Person.prototype);
-
-function JapaneseGirl(name, age) {
-  Japanese.call(this, name, age);
-  this.girl = true;
-}
-
-JapaneseGirl.prototype = Object.create(Japanese.prototype);
-
-function JapaneseBoy(name, age) {
-  Japanese.call(this, name, age);
-  this.boy = true;
-}
-
-JapaneseBoy.prototype = Object.create(Japanese.prototype);
-
-const bob = new Person("bob", 12);
-console.log(bob);
-
-bob.hello();
-bob.bye();
-
-const taro = new Japanese("taro", 43);
-console.log(taro);
-
-const hanako = new JapaneseGirl("hanako", 12);
-console.log(hanako);
-
-if(hanako.girl) {
-  console.log(`${hanako.name} is girl`);
-} else {
-  console.log(`${hanako.name} is not girl`);
-}
-
-const sabu = new JapaneseBoy("saburo", 23);
-console.log(sabu);
-
-if(sabu instanceof JapaneseBoy) {
-  console.log(`${sabu.name} is Japanese boy`);
-}
+).catch(() => {
+  console.log("dlroW");
+});
