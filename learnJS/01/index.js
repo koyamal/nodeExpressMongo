b717dab62d68e8b38d428e958e5e2f03ff528c5a
koyamal <tanakamaruko1121n@gmail.com>
2023-06-19 20:45:54 +0900

    Create bye

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -7,6 +7,10 @@ Person.prototype.hello = function() {
   console.log("Hello, I'm", this.name);
 }
 
+Person.prototype.bye = function() {
+  console.log("bye");
+}
+
 const bob = new Person("bob", 12);
 console.log(bob);
 
