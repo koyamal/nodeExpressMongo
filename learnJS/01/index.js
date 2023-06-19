0f05a6af13d21dabd52513ec9ad5a6438372a9ac
koyamal <tanakamaruko1121n@gmail.com>
2023-06-19 13:02:32 +0900

    Create prototype method

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -3,5 +3,11 @@ function Person (name, age) {
   this.age = age;
 }
 
+Person.prototype.hello = function() {
+  console.log("Hello, I'm", this.name);
+}
+
 const bob = new Person("bob", 12);
 console.log(bob);
+
+bob.hello();
