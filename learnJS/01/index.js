a54d37d7cbc5998181c878c022723772b801c67c
koyamal <tanakamaruko1121n@gmail.com>
2023-06-20 13:27:04 +0900

    Delete

--- learnJS/01/index.js
+++ learnJS/01/index.js
@@ -1,14 +1,3 @@
-// new Promise((resolve, reject) => {
-//   console.log("Hello ");
-//   resolve();
-// }).then(() => {
-//   console.log("World");
-//   return "Resolve";
-// }
-// ).catch(() => {
-//   console.log("dlroW");
-// });
-
 async function usePromise() {
   const res = await new Promise((resolve, reject) => {
     console.log("Hello ");
