6f298e2748296172846d2b5376f17434a513beb4
koyamal <tanakamaruko1121n@gmail.com>
2023-06-22 20:28:22 +0900

    Delete

--- learnJS/02/app.js
+++ learnJS/02/app.js
@@ -6,8 +6,6 @@ import v1ApiDoc from './api-v1/api-doc.js';
 const app = express();
 initialize({
   app,
-  // NOTE: If using yaml you can provide a path relative to process.cwd() e.g.
-  // apiDoc: './api-v1/api-doc.yml',
   apiDoc: v1ApiDoc,
   dependencies: {
     worldsService: v1WorldsService
